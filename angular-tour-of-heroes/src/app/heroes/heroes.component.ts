9b735b5d8670b3a2646ea646f9578a650a651a01
Vlad-Alexandru David <vlad.david@sitline.ro>
2022-12-07 15:24:20 +0200

    initial commit

---
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service'; 
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+  heroes : Hero[] = [];
+  constructor(private heroService: HeroService){}
+
+  ngOnInit(): void{
+    this.getHeroes();
+  }
+
+  getHeroes(): void{
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+}
