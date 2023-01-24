f45adb714be9168a24a280cb1627392c748e54a7
Vlad David <vladdavidapps@gmail.com>
2023-01-24 22:03:03 +0200

    finished tutorial

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,22 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HeroService } from '../hero.service'; 
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  heroes : Hero[] = [];
-  constructor(private heroService: HeroService){}
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
 
-  ngOnInit(): void{
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
     this.getHeroes();
   }
 
-  getHeroes(): void{
+  getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes);
   }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+
 }
--- angular-tour-of-heroes/src/app/messages/messages.component.ts
+++ angular-tour-of-heroes/src/app/messages/messages.component.ts
@@ -7,5 +7,7 @@ import { MessageService } from '../message.service';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent {
-  constructor(public messageService: MessageService){}
+
+  constructor(public messageService: MessageService) {}
+
 }
